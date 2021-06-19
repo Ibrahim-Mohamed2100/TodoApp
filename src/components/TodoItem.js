666946f0524605bc28a44359f18dec3264df4ab6
Ibrahim <ibrahim.mohamed.abdalah@gmail.com>
2021-06-19 22:01:13 +0200

    Implemented the add todo item within inputs area

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
-import { View, CheckBox, Body } from 'native-base';
-import { Ionicons } from '@expo/vector-icons';
+import { View, CheckBox, Body ,Icon} from 'native-base';
 
 const styles = StyleSheet.create({
   container: {
@@ -31,9 +30,71 @@ const propTypes = {
   onDelete: PropTypes.func.isRequired,
 };
 
-// the todo item goes here
 class TodoItem extends Component {
+  onTodoItemToggle = (todo, propAction) => {
+    propAction({
+      ...todo,
+      completed: !todo.completed,
+    });
+  };
 
+  render() {
+    const { todo, onUpdate, onDelete } = this.props;
+    console.log('inside todoItem todo', todo);
+
+    return (
+      <View style={styles.row}>
+        <View
+          style={{
+            flex: 1,
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingRight: 10,
+            paddingVertical: 5,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => this.onTodoItemToggle(todo, onUpdate)}
+            style={{
+              flex: 1,
+              width: '100%',
+              flexDirection: 'row',
+            }}
+          >
+            <CheckBox
+              checked={todo.completed}
+              onPress={() => this.onTodoItemToggle(todo, onUpdate)}
+            />
+            <Body
+              style={{
+                flex: 1,
+                justifyContent: 'flex-start',
+                alignItems: 'flex-start',
+                paddingLeft: 25,
+              }}
+            >
+              <Text
+                style={{
+                  color: todo.completed ? 'grey' : 'black',
+                  textDecorationLine: todo.completed ? 'line-through' : 'none',
+                }}
+              >
+                {todo.title}
+              </Text>
+            </Body>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => onDelete(todo)}
+            style={{ paddingLeft: 25, paddingRight: 15 }}
+          >
+            //TODO: add the specific icon name
+            <Icon name="add" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 TodoItem.propTypes = propTypes;
--- src/components/TodosContainer.js
+++ src/components/TodosContainer.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, StatusBar } from 'react-native';
+import { ScrollView, StyleSheet, FlatList, StatusBar } from 'react-native';
 import { View, Text } from 'native-base';
+// import {ReactiveBase,  ReactiveList } from '@appbaseio/reactivesearch-native';
 
 // import TodoModel from './../api/todos';
 import Header from 'components/Header';
 import COLORS from 'constants/Colors';
 import AddTodoButton from './AddTodoButton';
+import AddTodo from './AddTodo';
+import CONFIG from 'constants/Config';
+import TodoItem from 'components/TodoItem';
+import Utils from 'utils';
 
 const styles = StyleSheet.create({
     container: {
@@ -28,23 +33,90 @@ const styles = StyleSheet.create({
     },
 });
 
+//TODO:Implement ReactiveList to be used here
+//TODO: Fix ReactiveList maximum exceeded issue
 // will render todos based on the active screen: all, active or completed
 export default class TodosContainer extends React.Component {
+    state = {
+        addingTodo: false,
+    };
     componentDidMount() {
         // includes the methods for creation, updation and deletion
         // this.api = new TodoModel('react-todos');
     }
+
+    onAllData = (todos, streamData) => {
+        // merge streaming todos data along with current todos
+        const todosData = Utils.mergeTodos(todos, streamData);
+
+        // filter data based on "screen": [All | Active | Completed]
+        const filteredData = this.filterTodosData(todosData);
+
+        return (
+        <FlatList
+            style={{ width: '100%', top: 15 }}
+            data={filteredData}
+            keyExtractor={item => item._id}
+            renderItem={({ item: todo }) => (
+            <TodoItem todo={todo} onUpdate={this.api.update} onDelete={this.api.destroy} />
+            )}
+        />
+        );
+    };
+
+    filterTodosData = (todosData) => {
+        const { screen } = this.props;
+
+        switch (screen) {
+        case CONSTANTS.ALL:
+            return todosData;
+        case CONSTANTS.ACTIVE:
+            return todosData.filter(todo => !todo.completed);
+        case CONSTANTS.COMPLETED:
+            return todosData.filter(todo => todo.completed);
+        }
+
+        return todosData;
+    };
 
     render() {
         return (
             <View style={styles.container}>
                 <Header />
                 <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
-                <View style={styles.center}>
-                    <Text>Todos Container</Text>
-                    <AddTodoButton onPress={() => this.setState({ addingTodo: true })} />
+                <ScrollView>
+                {/* <ReactiveList
+                    componentId="ReactiveList"
+                    defaultQuery={() => ({
+                    query: {
+                        match_all: {},
+                    },
+                    })}
+                    stream
+                    onAllData={this.onAllData}
+                    dataField="title"
+                    showResultStats={false}
+                    pagination={false}
+                /> */}
+
+                {this.state.addingTodo ? (
+                    <View style={styles.row}>
+                    <AddTodo
+                        onAdd={(todo) => {
+                        this.setState({ addingTodo: false });
+                        console.log('inside TodoContainer:', todo);
+                        //TODO: implement the api CRUD to update DB
+                        // this.api.add(todo);
+                        }}
+                        onCancelDelete={() => this.setState({ addingTodo: false })}
+                        onBlur={() => this.setState({ addingTodo: false })}
+                    />
+                    </View>
+                ) : null}
+                </ScrollView>
+                <AddTodoButton onPress={() => this.setState({ addingTodo: true })} />
                 </View>
-            </View>
+                
         );
     }
 }
